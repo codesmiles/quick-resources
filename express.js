034bd9c292f211f3c68e57992cea4e7281085a87
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-03-02 14:20:47 +0100

    Update

---
+++ express.js
@@ -0,0 +1,22 @@
+//------------------------------------------------------------------------------
+// EXPRESS template
+// //------------------------------------------------------------------------------
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const app = express();
+
+// bodyParser.urlencoded({ extended: true });
+
+// app.use(bodyParser.json());
+
+// app.get('/', (req, res) => {
+//     // res.sendFile(__dirname + '/index.html');
+//     res.send('Hello World!');
+// });
+
+// app.listen(5000,()=>{
+//     console.log(`app is listening at port 5000`)
+// })
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
