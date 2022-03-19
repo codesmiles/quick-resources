6b3b9050dd9c597403d3f2bb956a26f0dd9900a6
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-03-19 23:11:11 +0100

    Express update

--- express.js
+++ express.js
@@ -1,13 +1,16 @@
 //------------------------------------------------------------------------------
 // EXPRESS template
 // //------------------------------------------------------------------------------
+// //jshint esversion:6
 // const express = require('express');
 // const bodyParser = require('body-parser');
+// const request = require("request");(Optional)
 // const app = express();
 
-// bodyParser.urlencoded({ extended: true });
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(bodyParser.json());
+// to be able to access ayour static folder
+// app.use(express.static(`public`));(middleware)
 
 // app.get('/', (req, res) => {
 //     // res.sendFile(__dirname + '/index.html');
