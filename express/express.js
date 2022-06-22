92f9eea4755a898fd6c4d9365ac03c489c26eab4
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-06-22 23:43:02 +0100

    update

--- express/express.js
+++ express/express.js
@@ -4,8 +4,9 @@
 //jshint esversion:6
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require("request");(Optional)
+const request = require("request");//(Optional)
 const validator = require("validator");
+const bcrypt = require("bcrypt");
 const ejs = require("ejs");
 const app = express();
 
@@ -40,6 +41,31 @@ mongoose.connect(url, function (err) {
 console.log(validator.isEmail(`mikedbci@fmail.com`));
 console.log(validator.isStrongPassword(`mikeAdbch@1`));
 //------------------------------------------------------
+
+      //------------------USING BCRYPT version 1----------------------
+      // generate salt to hashpassword
+      const salt = await bcrypt.genSalt(10);
+      // set password to a hashed password
+      password = await bcrypt.hash(password, salt);
+
+      //----------------------------------------------------
+
+      /////////////////bcrypt version 2//////////////////
+      // This has a fuction that will allow to write an stuff
+const saltRounds = 10;
+bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: hash,
+  });
+  newUser.save((err) => {
+    if (!err) {
+      res.render("secrets");
+    } else {
+      console.log(err);
+    }
+  });
+});
 
 
 // // Listen an d create port
