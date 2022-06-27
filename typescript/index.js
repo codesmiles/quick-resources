295bda022b149598736a9573d477af2c83069949
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-06-27 19:32:18 +0100

    Update on explicit types

--- typescript/index.js
+++ typescript/index.js
@@ -4,18 +4,43 @@ console.log("shit happens");
 var age = 30; // ignore this errors
 var isLearning = true;
 age = "michael"; //brings an error:Type 'string' is not assignable to type 'number'
-isLearning = "yes"; // give error:Type 'string' is not assignable to type 'boolean'.
+isLearning = "yes"; // gives error:Type 'string' is not assignable to type 'boolean'.
 isLearning = false; //gives no error
 age = 20; //gives no errors
 // you can also initially declare what type of value you want to assign to the variable
 var circ = function (diameter) {
     return diameter * Math.PI;
 };
-// console.log(circ(`hello`)) // gives an error:Argument of type 'string' is not assignable to parameter of type 'number'.
+// console.log(circ(`hello`)) // gives error:Argument of type 'string' is not assignable to parameter of type 'number'.
 console.log(circ(8.4));
 // arrays and objects
-var names = ["wahala", "local", "stuff"];
+// ARRAYS
+var names = ["wahala", "local", "stuff"]; //type of the value and vatriable is automatically fixed
 names.push("michael"); //no errors
-names.push(23); //will give:Argument of type 'number' is not assignable to parameter of type 'string'.
+names.push(23); //give error:Argument of type 'number' is not assignable to parameter of type 'string'. based on the type inference
 console.log(names);
 var num = [1, 3, 4, 6];
+num.push(21); //no err
+num.push("wahala"); //gives error :Argument of type 'string' is not assignable to parameter of type 'number'.
+console.log(num);
+//Declaring mixed array
+var numMixedStr = [12, "mike", 3, 5];
+numMixedStr.push("lion"); //no error
+numMixedStr.push(404); //no error
+console.log(numMixedStr);
+// OBJECTS
+var issues = {
+    name: "tochi",
+    problem: "annoying as fuck",
+    age: 25
+};
+// note you can't add extra properties to the object but you can override the object with a new object of the same properties structures
+console.log(issues);
+// PRE-INITIALIZING EXPLICIT TYPES so you cannot change the types along the line
+var char; // string
+var price; // number
+var isLoggedIn; // boolean
+char = "problem";
+price = 4000;
+isLoggedIn: false;
+// ARRAY
--- typescript/index.ts
+++ typescript/index.ts
@@ -6,21 +6,55 @@ let age = 30; // ignore this errors
 let isLearning = true;
 
 age = "michael";//brings an error:Type 'string' is not assignable to type 'number'
-isLearning = "yes"; // give error:Type 'string' is not assignable to type 'boolean'.
+isLearning = "yes"; // gives error:Type 'string' is not assignable to type 'boolean'.
 isLearning = false;//gives no error
 age = 20//gives no errors
 
 // you can also initially declare what type of value you want to assign to the variable
-const circ = (diameter: number)=>{ // defining the type in a function stating the parameter must be a number
-    return diameter*Math.PI;
+const circ = (diameter: number) => { // defining the type in a function stating the parameter must be a number
+    return diameter * Math.PI;
 }
-// console.log(circ(`hello`)) // gives an error:Argument of type 'string' is not assignable to parameter of type 'number'.
+// console.log(circ(`hello`)) // gives error:Argument of type 'string' is not assignable to parameter of type 'number'.
 console.log(circ(8.4));
 
 // arrays and objects
-let names =["wahala","local","stuff"];
+// ARRAYS
+let names = ["wahala", "local", "stuff"]; //type of the value and vatriable is automatically fixed
 names.push(`michael`);//no errors
-names.push(23)//will give:Argument of type 'number' is not assignable to parameter of type 'string'.
+names.push(23)//give error:Argument of type 'number' is not assignable to parameter of type 'string'. based on the type inference
 console.log(names);
 
-let num = [1,3,4,6]
+let num = [1, 3, 4, 6]
+num.push(21)//no err
+num.push(`wahala`)//gives error :Argument of type 'string' is not assignable to parameter of type 'number'.
+console.log(num)
+
+//Declaring mixed array
+let numMixedStr = [12, `mike`, 3, 5];
+numMixedStr.push(`lion`);//no error
+numMixedStr.push(404);//no error
+console.log(numMixedStr)
+
+// OBJECTS
+let issues = {//the type of the object and property sticked
+    name: `tochi`,
+    problem: `annoying as fuck`,
+    age: 25
+}
+// note you can't add extra properties to the object but you can override the object with a new object of the same properties structures
+console.log(issues)
+
+
+// PRE-INITIALIZING EXPLICIT TYPES so you cannot change the types along the line
+let char: string;// string
+let price: number;// number
+let isLoggedIn: boolean;// boolean
+
+char = `problem`;
+price = 4000;
+isLoggedIn:false;
+
+// ARRAY
+
+
+
